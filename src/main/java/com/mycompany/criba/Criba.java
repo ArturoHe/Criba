5f7be5f2e72b1d4af50d0e9a239268868eefd413
Arturo Herrera <joherrerar@unal.edu.co>
2023-08-17 10:32:02 -0500

    Se agregan comentarios para facil entendimiento del codigo

--- src/main/java/com/mycompany/criba/Criba.java
+++ src/main/java/com/mycompany/criba/Criba.java
@@ -1,3 +1,5 @@
+//Author: Arturo Herrera
+
 package com.mycompany.criba;
 
 import java.util.ArrayList;
@@ -6,37 +8,48 @@ public class Criba {
 
     public static void main(String[] args) {
 
-        int a = 1;
-        int b = 10000000;
-        ArrayList<Integer> primos = new ArrayList<>();
+        int a = 1; //Numero inicio NO CAMBIAR
+        
+        int b = 100; //Numero final (Cambiar este para el ultimo numero que queremos evaluar)
+        
+        ArrayList<Integer> primos = new ArrayList<>(); //ArrayList donde se guardan los numeros primos que se encuentran
 
+        //
+        //Aqui se evalua cada numero desde el 1 hasta el ultimo, en este caso b=100
         for (int i = a; i <= b; i++) {
 
             if (primos.isEmpty()) {
 
-                primos.add(i);
+                primos.add(i);//Anade el 1 para comenzar a evaluar los numeros
 
             } else {
 
+                //Variable que nos sirve para mirar si hay 1 solo divisor del numero evaluado
                 int cantidadDivisores = 0;
 
+                //Aqui se recorre la lista de primos buscando si son divisores del numero que esta siendo evaluado
                 for (int j = 0; j < primos.size(); j++) {
 
                     if (i % primos.get(j) == 0) {
 
+                        
                         cantidadDivisores++;
 
                     }
 
+                    //Si hay mas de 1 divisor se rompe el ciclo y evalua el numero siguiente (para optimizar tiempo y no gastar recursos)
                     if (cantidadDivisores > 1) {
 
                         break;
                     }
                 }
+                
+                //Si solo hay 1 divisor del numero, se agrega a los primos
                 if (cantidadDivisores == 1) {
 
                     primos.add(i);
 
+                    //Se imprime cada vez que encuentra un numero primo
                     System.out.println("Numero primo encontrado:" + i);
 
                 }
@@ -45,8 +58,10 @@ public class Criba {
 
         }
 
+        //Eliminamos el 1 de la lista de primos
         primos.remove(0);
 
+        //Se imprime la cantidad total de numeros primos que hay de 1 a b
         System.out.println("Cantidad total de numeros primos entre " + a + " y " + b + " = " + primos.size());
 
     }
